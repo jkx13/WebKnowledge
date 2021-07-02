f06aedb5459582c07784ec7bde4fce6cd96f2615
jkx13 <1219848990@qq.com>
2021-07-02 15:16:38 +0800

    update

--- React资料/react-demo/src/App.js
+++ React资料/react-demo/src/App.js
@@ -1,61 +1,110 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react';
+import 'antd/dist/antd.css';
+import './index.css';
+import {
+    Form,
+    Input,
+    Button,
+    Radio,
+    Select,
+    Cascader,
+    DatePicker,
+    InputNumber,
+    TreeSelect,
+    Switch,
+} from 'antd';
+export default function App() {
 
-export default class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            count: 0
-        }
-        console.log('constructor==初始化state=' + JSON.stringify(this.state));
-        console.log('End======================constructor========================');
-    }
-    static getDerivedStateFromProps(nextProps, prevState) {
-        console.log('getDerivedStateFromProps==static中访问不了this');
-        console.log('getDerivedStateFromProps==在初始化与update时都会执行用于取代componentWillReceiveProps');
-        console.log('nextProps==' + JSON.stringify(nextProps));
-        console.log('prevState==' + JSON.stringify(prevState));
+    const [componentSize, setComponentSize] = useState('default');
 
-        console.log('End======================getDerivedStateFromProps========================');
-        return prevState;
-    }
+    const onFormLayoutChange = ({ size }) => {
+        setComponentSize(size);
+    };
 
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log('shouldComponentUpdate==是否需要更新组件');
-        console.log('nextProps==' + JSON.stringify(nextProps));
-        console.log('nextState==' + JSON.stringify(nextState));
-        console.log('End======================shouldComponentUpdate========================');
-
-        return true;//更新
+    const onFinish = (values) => {
+        console.log('GET==' + JSON.stringify(values));
     }
 
-    componentDidMount() {
-        console.log('componentDidMount==组件挂载后调用');
-        console.log('componentDidMount==可以请求或订阅');
+    return (
+        <div className="app-box">
 
-        console.log('End======================componentDidMount========================');
-    }
+            <Form
+                labelCol={{
+                    span: 4,
+                }}
+                wrapperCol={{
+                    span: 14,
+                }}
+                layout="horizontal"
+                initialValues={{
+                    size: componentSize,
+                }}
+                onValuesChange={onFormLayoutChange}
+                size={componentSize}
+                onFinish={onFinish}
+            >
+                <Form.Item label="Form Size" name="size">
+                    <Radio.Group>
+                        <Radio.Button value="small">Small</Radio.Button>
+                        <Radio.Button value="default">Default</Radio.Button>
+                        <Radio.Button value="large">Large</Radio.Button>
+                    </Radio.Group>
+                </Form.Item>
+                <Form.Item label="Input">
+                    <Input />
+                </Form.Item>
+                <Form.Item label="Select">
+                    <Select>
+                        <Select.Option value="demo">Demo</Select.Option>
+                    </Select>
+                </Form.Item>
+                <Form.Item label="TreeSelect">
+                    <TreeSelect
+                        treeData={[
+                            {
+                                title: 'Light',
+                                value: 'light',
+                                children: [
+                                    {
+                                        title: 'Bamboo',
+                                        value: 'bamboo',
+                                    },
+                                ],
+                            },
+                        ]}
+                    />
+                </Form.Item>
+                <Form.Item label="Cascader">
+                    <Cascader
+                        options={[
+                            {
+                                value: 'zhejiang',
+                                label: 'Zhejiang',
+                                children: [
+                                    {
+                                        value: 'hangzhou',
+                                        label: 'Hangzhou',
+                                    },
+                                ],
+                            },
+                        ]}
+                    />
+                </Form.Item>
+                <Form.Item label="DatePicker">
+                    <DatePicker />
+                </Form.Item>
+                <Form.Item label="InputNumber">
+                    <InputNumber />
+                </Form.Item>
+                <Form.Item label="Switch">
+                    <Switch />
+                </Form.Item>
+                <Form.Item label="Button">
+                    <Button>Button</Button>
+                </Form.Item>
+            </Form>
 
-    getSnapshotBeforeUpdate() {
-        console.log('getSnapshotBeforeUpdate==用于获取最新DOM数据');
-        console.log('End======================getSnapshotBeforeUpdate========================');
-    }
+        </div>
+    )
 
-    componentDidUpdate() {
-        console.log('componentDidUpdate==组件更新后调用');
-        console.log('End======================componentDidUpdate========================');
-    }
-
-    componentWillUnmount() {
-        console.log('componentWillUnmount==组件将要销毁,用于取消订阅或定时器等');
-        console.log('End======================componentWillUnmount========================');
-    }
-    render() {
-        console.log('render==渲染组件函数');
-        console.log('End======================render========================');
-        return (
-            <div>
-                hello
-            </div>
-        )
-    }
 }
